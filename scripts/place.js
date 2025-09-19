d9a0e0c9b6316c2294cb9af808a5fd7327e048f1
otuekongakpan <oakpan2@byupathway.edu>
2025-09-19 20:11:58 +0100

    edit ini

--- scripts/place.js
+++ scripts/place.js
@@ -12,15 +12,23 @@ document.addEventListener("DOMContentLoaded", function () {
     modifiedSpan.textContent = lastModified.toLocaleDateString("en-US", options);
     }
 
+    function calculateWindChill(temp, speed) {
+        if (temp <= 50 && speed > 3) {
+            return 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+        }
+        else {
+            return "N/A";
+        }
+    }
     const windchill = document.querySelector("#wind-chill");
     if (windchill) {
-        const temp = parseFloat(document.querySelector("#temperature").textContent);
-        const speed = parseFloat(document.querySelector("#wind-speed").textContent);
-        if (temp <= 50 && speed > 3) {
-            const chill = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
-            windchill.textContent = chill.toFixed(2) + " °F";
-        } else {
-            windchill.textContent = "N/A";
+        const tempElement = document.querySelector("#temperature");
+        const speedElement = document.querySelector("#wind-speed");
+        if (tempElement && speedElement) {
+            const temp = parseFloat(tempElement.textContent);
+            const speed = parseFloat(speedElement.textContent);
+            const chill = calculateWindChill(temp, speed);
+            windchill.textContent = (typeof chill === "number") ? chill.toFixed(2) + " °F" : chill;
         }
     }
 });
