9f67248d39c3ad01a0b8502af0c31d3c1dac7ca8
otuekongakpan <oakpan2@byupathway.edu>
2025-10-06 09:46:10 +0100

    css commit

---
+++ scripts/form.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  const yearSpan = document.querySelector("#currentyear");
+  if (yearSpan) {
+    yearSpan.textContent = new Date().getFullYear();
+  }
+
+  const modifiedSpan = document.querySelector("#lastModified");
+  if (modifiedSpan) {
+    const lastModified = new Date(document.lastModified);
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    modifiedSpan.textContent = lastModified.toLocaleDateString("en-US", options);
+    }
+});
